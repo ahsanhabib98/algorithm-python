56b1fd023b7b390253fcec054df9ebfe6e50cafb
Ahsan Habib <ahredoan@gmail.com>
2023-01-16 05:33:10 +0600

    Implement dfs for undirect fraph

---
+++ dfs_undirect_graph.py
@@ -0,0 +1,43 @@
+adj_list = {
+    "A": ["B"],
+    "B": ["A", "D", "E"],
+    "C": ["F"],
+    "D": ["B"],
+    "E": ["B"],
+    "F": ["C"]
+}
+    
+color = {}    # W, G, B
+parent = {}
+traverse_time = {}    # [start, end]
+traverse = []
+
+for node in adj_list.keys():
+    color[node] = "W"
+    parent[node] = None
+    traverse_time[node] = [-1, -1]
+    
+time = 0
+def dfs(u):
+    global time
+    color[u] = "G"
+    traverse_time[u][0] = time
+    traverse.append(u)
+    time += 1
+    for v in adj_list[u]:
+        if color[v] == 'W':
+            parent[v] = u
+            dfs(v)
+    color[u] = "B"
+    traverse_time[u][1] = time
+    time += 1
+
+for u in adj_list.keys():
+    if color[u] == "W":
+        dfs(u)
+print(traverse)
+print(parent)
+print(traverse_time)
+
+for node in adj_list.keys():
+    print(node, " -> ", traverse_time[node])
