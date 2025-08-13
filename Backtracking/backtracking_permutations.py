e25efd615387d6c8c3edf5c8c45457cfd1627124
Ahsan Habib <ahabib2@islander.tamucc.edu>
2025-08-13 18:07:45 -0500

    Implement alternate approach to solve permutations

--- Backtracking/backtracking_permutations.py
+++ Backtracking/backtracking_permutations.py
@@ -10,3 +10,18 @@ class Solution:
                 p_copy.insert(i, nums[0])
                 res.append(p_copy)
         return res
+
+# Alternate approach
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        if len(nums) == 1:
+            return [nums[:]]
+        for i in range(len(nums)):
+            num = nums.pop(0)
+            perms = self.permute(nums)
+            for p in perms:
+                p.append(num)
+            res.extend(perms)
+            nums.append(num)
+        return res
