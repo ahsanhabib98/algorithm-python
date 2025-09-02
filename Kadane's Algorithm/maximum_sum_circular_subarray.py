0edf73e1cabebcf4fc62ec81868156bfd88fcd27
Ahsan Habib <ahabib2@islander.tamucc.edu>
2025-09-02 18:24:12 -0500

    Implement maximum sum cricular sub array

---
+++ Kadane's Algorithm/maximum_sum_circular_subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        globalMax, globalMin = nums[0], nums[0]
+        curMax, curMin = 0, 0
+        total = 0
+        for num in nums:
+            total += num
+            curMax = max(curMax + num, num)
+            curMin = min(curMin + num, num)
+            globalMax = max(globalMax, curMax)
+            globalMin = min(globalMin, curMin)
+        return max(globalMax, total - globalMin) if globalMax > 0 else globalMax
